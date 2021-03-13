3359440a76c247884704c31c4f090ea1dd8718bc
jramos19-dev <jramos19@southtexascollege.edu>
2021-03-12 20:18:25 -0600

    fixed all the error handling and error messages

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -18,15 +18,23 @@
           class="flex items-center h-12 px-4 mt-2 rounded focus:outline-none focus:ring-2 text-coolGray-900"
           v-model="name"
         />
+        <p class="capitalize text-red-500 text-sm text-left">
+          {{ nameError }}
+        </p>
         <label for="username" class="self-start text-xs font-semibold"
           >Email</label
         >
+        {{ email }}
         <input
           id="username"
           type="text"
           class="flex items-center h-12 px-4 mt-2 rounded focus:outline-none focus:ring-2 text-coolGray-900"
           v-model="email"
         />
+        <p class="capitalize text-red-500 text-sm text-left">
+          {{ errorEmail }}
+        </p>
+
         <label for="password" class="self-start mt-3 text-xs font-semibold"
           >Password</label
         >
@@ -36,6 +44,25 @@
           class="flex items-center h-12 px-4 mt-2 rounded focus:outline-none focus:ring-2 text-coolGray-900"
           v-model="password"
         />
+        <p class="capitalize text-red-500 text-sm text-left">
+          {{ errorPassword }}
+        </p>
+        <label
+          for="passwordConfirmation"
+          class="self-start mt-3 text-xs font-semibold"
+          >Confirm Password</label
+        >
+        <input
+          v-if="form == 'signUp'"
+          id="passwordConfirmation"
+          type="password"
+          class="flex items-center h-12 px-4 mt-2 rounded focus:outline-none focus:ring-2 text-coolGray-900"
+          v-model="passwordConfirmation"
+        />
+        <p class="capitalize text-red-500 text-sm text-left">
+          {{ errorPasswordConfirmation }}
+        </p>
+
         <button
           v-if="form == 'login'"
           @click="login"
@@ -56,17 +83,24 @@
 </template>
 
 <script setup>
-import { ref, defineProps } from "vue"
+import { defineProps } from "vue"
 import { useRouter } from "vue-router"
+import { useField } from "vee-validate"
 import { signIn, signUp, googlePopup, auth } from "../helpers/useAuth"
 import { isError, msg } from "../helpers/useError"
+import * as yup from "yup"
 
 const router = useRouter()
 const login = async () => {
   try {
-    await signIn(email.value, password.value)
-    isError.value = false
-    router.push("/")
+    if (emailMeta.valid && passwordMeta.valid) {
+      await signIn(email.value, password.value)
+      isError.value = false
+      router.push("/")
+    } else {
+      isError.value = true
+      msg.value = "invalid Values"
+    }
   } catch (error) {
     isError.value = true
     msg.value = "UserName or Password is incorrect"
@@ -75,12 +109,22 @@ const login = async () => {
 }
 const register = async () => {
   try {
-    await signUp(email.value, password.value)
-    const user = auth().currentUser
+    if (
+      nameMeta.valid &&
+      emailMeta.valid &&
+      passwordMeta.valid &&
+      passwordConfirmationMeta.valid
+    ) {
+      await signUp(email.value, password.value)
+      const user = auth().currentUser
 
-    await user.updateProfile({ displayName: name.value })
-    isError.value = false
-    router.push("/")
+      await user.updateProfile({ displayName: name.value })
+      isError.value = false
+      router.push("/")
+    } else {
+      isError.value = true
+      msg.value = "invalid Values"
+    }
   } catch (error) {
     isError.value = true
     msg.value = "There was an Authentication Error"
@@ -99,9 +143,33 @@ const google = async () => {
   }
 }
 
-const email = ref("")
-const password = ref("")
-const name = ref("")
+const { value: email, errorMessage: errorEmail, meta: emailMeta } = useField(
+  "email",
+  yup.string().required().email()
+)
+const {
+  value: password,
+  errorMessage: errorPassword,
+  meta: passwordMeta,
+} = useField("password", yup.string().required().min(8))
+
+const passwordConfirmationFn = () => {
+  if (password.value === passwordConfirmation.value) {
+    return true
+  }
+  return "password doesnt match"
+}
+
+const {
+  value: passwordConfirmation,
+  errorMessage: errorPasswordConfirmation,
+  meta: passwordConfirmationMeta,
+} = useField("passwordConfirmation", passwordConfirmationFn)
+
+const { value: name, errorMessage: nameError, meta: nameMeta } = useField(
+  "name",
+  yup.string().required()
+)
 
 defineProps({
   form: {
