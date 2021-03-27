07737b9a021a2c36871bb4a1c5bdc4eccf3dc537
jramos19-dev <jramos19@southtexascollege.edu>
2021-03-27 01:45:40 -0500

    added image to  secret page and some designs

--- src/views/Secret.vue
+++ src/views/Secret.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>This is a Secret Page</h1>
+  <div class="container ustify-items-center mx-auto mt-8">
+    <div class="container bg-gray-500">
+      <h1 class="text-gray-100 text-3xl text-center">This is a Secret Page</h1>
+    </div>
+
+    <img src="../assets/hush.jpg" class="containte" />
+  </div>
 </template>
